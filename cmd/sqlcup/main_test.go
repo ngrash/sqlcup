cedd8c16df560536c1252e0a1e04552e4194fab3
Nico Grashoff <ngrash@gmail.com>
2023-02-05 14:58:24 +0100

    Reformat smart column test table

--- cmd/sqlcup/main_test.go
+++ cmd/sqlcup/main_test.go
@@ -9,71 +9,14 @@ var smartColTests = map[string]struct {
 	col column
 	err error
 }{
-	"@id": {
-		col: column{
-			Name:       "id",
-			Type:       "INTEGER",
-			Constraint: "PRIMARY KEY",
-			ID:         true,
-		},
-	},
-	"col_id@id": {
-		col: column{
-			Name:       "col_id",
-			Type:       "INTEGER",
-			Constraint: "PRIMARY KEY",
-			ID:         true,
-		},
-	},
-	"primary_key@text@id": {
-		col: column{
-			Name:       "primary_key",
-			Type:       "TEXT",
-			Constraint: "NOT NULL PRIMARY KEY",
-			ID:         true,
-		},
-	},
-	"col@text": {
-		col: column{
-			Name:       "col",
-			Type:       "TEXT",
-			Constraint: "NOT NULL",
-			ID:         false,
-		},
-	},
-	"col@text@null": {
-		col: column{
-			Name:       "col",
-			Type:       "TEXT",
-			Constraint: "",
-			ID:         false,
-		},
-	},
-	"col@text@unique": {
-		col: column{
-			Name:       "col",
-			Type:       "TEXT",
-			Constraint: "NOT NULL UNIQUE",
-			ID:         false,
-		},
-	},
-	"col@int": {
-		col: column{
-			Name:       "col",
-			Type:       "INTEGER",
-			Constraint: "NOT NULL",
-			ID:         false,
-		},
-	},
-	"col@datetime": {
-		col: column{
-			Name:       "col",
-			Type:       "DATETIME",
-			Constraint: "NOT NULL",
-			ID:         false,
-		},
-		err: nil,
-	},
+	"@id":                 {col: column{Name: "id", Type: "INTEGER", Constraint: "PRIMARY KEY", ID: true}},
+	"col_id@id":           {col: column{Name: "col_id", Type: "INTEGER", Constraint: "PRIMARY KEY", ID: true}},
+	"primary_key@text@id": {col: column{Name: "primary_key", Type: "TEXT", Constraint: "NOT NULL PRIMARY KEY", ID: true}},
+	"col@text":            {col: column{Name: "col", Type: "TEXT", Constraint: "NOT NULL", ID: false}},
+	"col@text@null":       {col: column{Name: "col", Type: "TEXT", Constraint: "", ID: false}},
+	"col@text@unique":     {col: column{Name: "col", Type: "TEXT", Constraint: "NOT NULL UNIQUE", ID: false}},
+	"col@int":             {col: column{Name: "col", Type: "INTEGER", Constraint: "NOT NULL", ID: false}},
+	"col@datetime":        {col: column{Name: "col", Type: "DATETIME", Constraint: "NOT NULL", ID: false}, err: nil},
 }
 
 func TestParseSmartColumnDefinition(t *testing.T) {
